a5dec0f22e7c07864e1f039d0c12ec835cace5c5
Jeff Tranter <tranter@pobox.com>
2021-06-12 09:14:26 -0400

    Enter more code.

--- sbc/software/sysmon.asm
+++ sbc/software/sysmon.asm
@@ -79,6 +79,81 @@ INPUT2: IN      A,(CDATA) ;GET BYTE
 ;
 ; GET CONSOLE-INPUT STATUS
 ;
+INSTAT: IN      A,(CSTAT)
+        AND     INMSK
+        RET
+;
+; TOGGLE LIST OUTPUT WITH CONTROL-P
+;
+SETLST: LD      A,(PORTN) ;CHECK FLAG
+        CPL               ;INVERT
+        LD      (PORTN),A ;SAVE
+        JR      INPUTT    ;NEXT BYTE
+;
+; CONSOLE INPUT ROUTINE
+;
+OUTT:   PUSH    AF
+        LD      A,(PORTN) ;WHERE?
+        OR      A       ;ZERO?
+        JR      NZ,LOUT ;LIST OUTPUT
+OUT2:   CALL    INSTAT  ;INPUT?
+        JR      Z,OUT4  ;NO
+        CALL    INPUT2  ;GET INPUT
+        CP      CTRS    ;FREEZE?
+        JR      NZ,OUT2 ;NO
+OUT3:   CALL    INPUTT  ;INPUT?
+        CP      CTRQ    ;RESUME?
+        JR      NZ,OUT3 ;NO
+        JR      OUT2
+;
+OUT4:   IN      A,(CSTAT)  ;GET STATUS
+        AND     OMSK
+        JR      Z,OUT2     ;NOT READY
+        POP     AF
+        OUT     (CDATA),A  ;SEND DATA
+;
+; LIST OUTPUT ROUTINE
+; SEND TO CONSOLE TOO
+;
+LOUT:   CALL    INSTAT  ;INPUT?
+        CALL    NZ,INPUT2 ;YES, GET IT
+;
+        IN      A,(LSTAT) ;CHECK STATUS
+        AND     OMSK
+        JR      Z,LOUT    ;NOT READY
+        POP     AF
+        OUT     (LDATA),A ;SEND DATA
+        OUT     (CDATA),A ;CONSOLE TOO
+        AND     7FH       ;MASK PARITY
+;
+; ADD TIME DELAY AFTER CARRIAGE RETURN
+;
+        CP      CR      ;CARRIAGE RET?
+        RET     NZ      ;NO
+        PUSH    DE      ;USE D,E
+        LD      D,30 * NNULS
+OUTCR:  LD      E,250
+OUTCR2: DEC     E
+        JR      NZ,OUTCR2 ;INNER LOOP
+        DEC     D
+        JR      NZ,OUTCR ;OUTER LOOP
+        POP     DE       ;RESTORE
+        RET
+;
+; CONTINUATION OF COLD START
+;
+COLD:   LD      SP,STACK
+;
+; INITIALIZE I/O PORTS
+;
+        LD      A,3
+
+
+
+
+
+
+
         LD      E,(HL)  ;LOW BYTE
         INC     HL
         LD      D,(HL)  ;HIGH BYTE
@@ -89,3 +164,51 @@ INPUT2: IN      A,(CDATA) ;GET BYTE
 ;
 TABLE:  DW      ASCII   ;A, DUMP,LOAD
         DW      ERROR   ;B
+        DW      CALLS   ;C, SUBROUTINE
+        DW      DUMP    ;D, DUMP
+        DW      ERROR   ;E
+        DW      FILL    ;F, MEMORY
+        DW      GO      ;G,GO
+        DW      HMATH   ;H, HEX MATH
+        DW      IPORT   ;I, PORT INPUT
+        DW      JUST    ;J, MEMORY TEST
+        DW      ERROR   ;K
+        DW      LOAD    ;L, LOAD
+        DW      MOVE    ;M, MEMORY
+        DW      ERROR   ;N
+        DW      OPORT   ;O, PORT OUTPUT
+        DW      ERROR   ;P
+        DW      ERROR   ;Q
+        DW      REPL    ;R, REPLACE
+        DW      SEARCH  ;S, MEMORY
+        DW      ERROR   ;T
+        DW      ERROR   ;U
+        DW      VERM    ;V, VERIFY MEM
+        DW      ERROR   ;W
+        DW      REGS    ;X, STK PNTR
+        DW      ERROR   ;Y
+        DW      ZERO    ;Z, MEMORY
+;
+; INPUT A LINE FROM CONSOLE AND PUT IT
+; INTO THE BUFFER. CARRIAGE RETURN ENDS
+; THE LINE. RUBOUT OR ^H CORRECTS LAST
+; ENTRY. CONTROL-X RESTARTS LINE.
+; OTHER CONTROL CHARACTERS ARE IGNORED.
+;
+INPLN:  LD      A,'>'   ;PROMPT
+        CALL    OUTT
+INPL1:  LD      HL,IBUFF  ;BUFFER ADDR
+        LD      (IBUFP),HL  ;SAVE POINTER
+        LD      C,0     ;COUNT
+INPLI:  CALL    INPUTT  ;CONSOLE CHAR
+        CP      ' '     ;CONTROL?
+        JR      C,INPLC ;YES
+        CP      DEL     ;DELETE?
+        JR      Z,INPLB ;YES
+        CP      'Z'+1   ;UPPER CASE?
+        JR      C,INPL3 ;YES
+        AND     5FH     ;MAKE UPPER
+INPL3:  LD      (HL),A  ;INTO BUFFER
+        LD      A,32    ;BUFFER SIZE
+        CP      C       ;FULL
+        JR      Z,INPLI ;YES, LOOP
