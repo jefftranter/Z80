20d21ccf1ff6b76c183589e23e90a39469b57cb7
Jeff Tranter <tranter@pobox.com>
2021-11-04 19:43:46 -0400

    Add more source code.

--- sbc/software/patb.asm
+++ sbc/software/patb.asm
@@ -1,8 +1,17 @@
 ; Port of Palo Alto Tiny BASIC Version Three, by Li-Chen Wang.
 ; Source taken from PCC's Reference Book of Personal and Home Computing.
 ; Adapted to ASL assembler and ported to my Z80 SBC.
+;
+; Possible enhancements:
+; - Use more standard statement separator ":" rather than ";".
+; - Use more standard operator "<>" rather than "#" for not equal.
+; - Make error messages longer/more descriptive.
+; - Add support for more commands, e.g. PEEK, POKE, USR().
+; - Convert from 8080 to Z80 mnemonics.
 
         CPU     8080
+
+CR      EQU     0DH             ; CARRIAGE RETURN
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
@@ -18,7 +27,7 @@
 ;  *** MEMORY USAGE ***
 ;
 ;  0080-01FF  ARE FOR VARIABLES, INPUT LINE, AND STACK
-;  2000-3FFF  ARE FOR TINY BASIC TExT & ARRAY
+;  2000-3FFF  ARE FOR TINY BASIC TEXT & ARRAY
 ;  F000-F7FF  ARE FOR TBI CODE
 ;
 
@@ -32,26 +41,26 @@ BOTROM  EQU     0F000H
 ;  DEFINE VARIABLES, BUFFER, AND STACK IN RAM
 
         ORG     BOTSCR
-KEYWRD  DS      1       ; WAS INIT DONE?
-TXTLMT  DS      1       ; ->LIMIT OF TEXT AREA
-VARBGN  DS      2*26    ; TB VARIABLES A-Z
-CURRNT  DS      2       ; POINTS TO CURRENT LINE
-STKGOS  DS      2       ; SAVES SP IN 'GOSUB'
-VARNXT  DS      0       ; TEMP STORAGE
-STKINP  DS      2       ; SAVES SP IN 'INPUT'
-LOPVAR  DS      2       ; 'FOR' LOOP SAVE AREA
-LOPINC  DS      2       ; INCREMENT
-LOPLMT  DS      2       ; LIMIT
-LOPLN   DS      2       ; LINE NUMBER
-LOPPT   DS      2       ; TEXT POINTER
-RANPNT  DS      2       ; RANDOM NUMBER POINTER
-        DS      1       ; EXTRA BYTE FOR BUFFER
-BUFFER  DS      132     ; INPUT BUFFER
-BUFEND  DS      0       ; BUFFER ENDS
-        DS      4       ; EXTRA BYTES FOR STACK
-STKLMT  DS      0       ; SOFT LIMIT FOR STACK
+KEYWRD  DS      1               ; WAS INIT DONE?
+TXTLMT  DS      1               ; ->LIMIT OF TEXT AREA
+VARBGN  DS      2*26            ; TB VARIABLES A-Z
+CURRNT  DS      2               ; POINTS TO CURRENT LINE
+STKGOS  DS      2               ; SAVES SP IN 'GOSUB'
+VARNXT  DS      0               ; TEMP STORAGE
+STKINP  DS      2               ; SAVES SP IN 'INPUT'
+LOPVAR  DS      2               ; 'FOR' LOOP SAVE AREA
+LOPINC  DS      2               ; INCREMENT
+LOPLMT  DS      2               ; LIMIT
+LOPLN   DS      2               ; LINE NUMBER
+LOPPT   DS      2               ; TEXT POINTER
+RANPNT  DS      2               ; RANDOM NUMBER POINTER
+        DS      1               ; EXTRA BYTE FOR BUFFER
+BUFFER  DS      132             ; INPUT BUFFER
+BUFEND  DS      0               ; BUFFER ENDS
+        DS      4               ; EXTRA BYTES FOR STACK
+STKLMT  DS      0               ; SOFT LIMIT FOR STACK
         ORG     TOPSCR
-STACK   DS      0       ; STACK STARTS HERE
+STACK   DS      0               ; STACK STARTS HERE
         ORG     BOTRAM
 TXTONF  DS      2
 TEXT    DS      2
@@ -64,3 +73,92 @@ TEXT    DS      2
 
         ORG     BOTROM
 INIT    LXI     SP,STACK
+        CALL    CRLF
+        LXI     H,KEYWRD        ; AT POWER ON KEYWRD IS
+        MVI     A,0C3H          ; PROBABLY NOT C3
+        CMP     M
+        JZ      TELL            ; IT IS C3, CONTINUE
+        MOV     M,A             ; AND SET DEFAULT VALUE
+        SHLD    TXTLMT          ; IN 'TXTLMT'
+        MVI     A,BOTROM>>8     ; INITIALIZE RANPNT
+        STA     RANPNT+1
+PURGE   LXI     H,TEXT+4        ; PURGE TEXT AREA
+        SHLD    TXTUNF
+        MVI     H,0FFH
+        SHLD    TEXT
+TELL    LXI     D,MSG           ; TELL USER
+        CALL    PRTSTG          ; ***********************
+        JMP     RSTART          ; ***** JMP USER-INIT ***
+MSG     DB      "TINY "         ; ***********************
+        DB      "BASIC"
+
+        DB      " V3.0",CR
+OK      DB      "OK",CR
+WHAT    DB      "WHAT?",CR
+HOW     DB      "HOW?",CR
+SORRY   DB      "SORRY",CR
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; *** DIRECT COMMAND / TEXT COLLECTER
+;
+; TBI PRINTS OUT "OK(CR)", AND THEN IT PROMPTS ">" AND READS A LINE.
+; IF THE LINE STARTS WITH A NON-ZERO NUMBER, THIS NUMBER IS THE LINE
+; NUMBER. THE LINE NUMBER (IN 16 BIT BINARY) AND THE REST OF THE LINE
+; (INCLUDING THE CR) IS STORED IN THE MEMORY. IF A LINE WITH THE SAME
+; LINE NUMBER IS ALREADY THERE, IT IS REPLACED BY THE NEW ONE. IF THE
+; REST OF THE LINE CONSISTS OF A CR ONLY, IT IS NOT STORED AND ANY
+; EXISTING LINE WITH THE SAME LINE NUMBER IS DELETED.
+;
+; AFTER A LINE IS INSERTED, REPLACED, OR DELETED, THE PROGRAM LOOPS
+; BACK AND ASK FOR ANOTHER LINE. THIS LOOP WILL BE TERMINATED WHEN IT
+; READS A LINE WITH ZERO OR NO LINE NUMBER; AND CONTROL IS TRANSFERED
+; TO "DIRECT".
+;
+; TINY BASIC PROGRAM SAVE AREA STARTS AT THE MEMORY LOCATION LABELED
+; "TEXT". THE END OF TEST IS MARKED BY 2 BYTES XX FF. FOLLOWING
+; THESE ARE 2 BYTES RESERVED FOR THE ARRAY ELEMENT @(0). THE CONTENT
+; OF LOCATION LABELED "TXTUNF" POINTS TO ONE AFTER @(0).
+;
+; THE MEMORY LOCATION "CURRNT" POINTS TO THE LINE NUMBER THAT IS
+; CURRENTLY BEING EXECUTED. WHILE WE ARE IN THIS LOOP OR WHILE WE
+; ARE INTERPRETING A DIRECT COMMENT (SEE NEXT SECTION), "CURRNT"
+; SHOULD POINT TO A 0.
+;
+RSTART  LXI     SP,STACK        ; RE-INITIALIZE STACK
+        LXI     H,ST1+1         ; LITERAL 0
+        SHLD    CURRNT          ; CURRNT->LINE # = 0
+ST1     LXI     H,0
+        SHLD    LOPVAR
+        SHLD    STKGOS
+        LXI     D,OK            ; DE->STRING
+        CALL    PRTSTG          ; PRINT STRING UNTIL CR
+ST2     MVI     A,'>'           ; PROMPT '>' AND
+        CALL    GETLN           ; READ A LINE
+        PUSH    D               ; DE->END OF LINE
+        LXI     D,BUFFER        ; DE->BEGINNING OF LINE
+        CALL    TSTNUM          ; TEST IF IT IS A NUMBER
+        CALL    IGNBLK
+        MOV     A,H             ; HL=VALUE OF THE # OR
+        ORA     L               ; 0 IF NO # WAS FOUND
+        POP     B               ; BC->END OF LINE
+        JZ      DIRECT
+        DCX     D               ; BACKUP DE AND SAVE
+        MOV     A,H             ; VALUE OF LINE # THERE
+        STAX    D
+        DCX     D
+        MOV     A,L
+        STAX    D
+        PUSH    B               ; BC,DE->BEGIN, END
+        PUSH    D
+        MOV     A,C
+        SUB     E
+        PUSH    PSW             ; A=# OF BYTES IN LINE
+        CALL    FNDLN           ; FIND THIS LINE IN SAVE
+        PUSH    D               ; AREA, DE->SAVE AREA
+        JNZ     ST3             ; NZ:NOT FOUND. INSERT
+        PUSH    D               ; Z:FOUND. DELETE IT
+        CALL    FNDNXT          ; SET DE->NEXT LINE
+        POP     B               ; BC->LINE TO BE DELETED
+        LHLD    TXTUNF          ; HL->UNFILLED SAVE AREA
+
