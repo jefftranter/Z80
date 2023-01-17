a894d21d3d38c6c17e1db7fe1031f8aa2d312af4
Jeff Tranter <tranter@pobox.com>
2023-01-17 16:30:38 -0500

    Add clear screen program.

---
+++ CPUvilleZ80/cls.asm
@@ -0,0 +1,13 @@
+; Simple clear screen program. Assumes ANSI terminal.
+
+        cpu     8080
+        org     100h
+bdos    equ     0005h           ; BDOS entry point
+start:  mvi     c,9             ; BDOS function: output string
+        lxi     d,msg           ; Address of msg
+        call    bdos
+        ret                     ; Return to CCP
+
+; VT100/ANSI clear screen, cursor home
+msg:    db      01BH,"[2J",01BH,"[H$"
+        end
