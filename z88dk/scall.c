c7d6af45e6247297ef2d85e9d95780db02260989
Jeff Tranter <tranter@pobox.com>
2024-12-16 19:33:27 -0500

    Bug fixes. It is working now.

--- z88dk/scall.c
+++ z88dk/scall.c
@@ -19,24 +19,20 @@ extern uint8_t *CALLNO;
 int scall(uint8_t request, uint8_t *a, uint16_t *bc, uint16_t *de, uint16_t *hl)
 {
     Z80_registers regs;
-    register_usage usage;
 
     // Write system call number after RST instruction
-    *CALLNO = request;
+    z80_bpoke(&CALLNO, request);
 
     // TODO: We could pass NULL to indicate parameters that are not needed?
-    
+
     // Set up register parameters.
     regs.Bytes.A = *a;
     regs.UWords.BC = *bc;
     regs.UWords.DE = *de;
     regs.UWords.HL = *hl;
 
-    // Specify registers used.
-    usage = REGS_MAIN;
-
     // Call SYSCALL routine below.
-    AsmCall(SYSCALL, &regs, usage, usage);
+    AsmCall(&SYSCALL, &regs, REGS_MAIN, REGS_MAIN);
 
     // Get back register values to return.
     *a = regs.Bytes.A;
@@ -53,7 +49,7 @@ _SYSCALL:
         rst     $38     ; System call
 _CALLNO:
         db      $00     ; Call # goes here
-        ret             
+        ret
 #endasm
 
 int main()
@@ -63,6 +59,7 @@ int main()
     uint16_t bc;
     uint16_t de;
     uint16_t hl;
+    char *s;
 
     printf("\nSystem Call Demo\n");
 
@@ -81,13 +78,14 @@ int main()
     printf("de = %04x\n", de);
     printf("hl = %04x\n", hl);
 
-    a = 0; // Also try A=1 for abort
+    s = "\nThis is a test.\n\xcrc180";
+    a = 0;
     bc = 0;
     de = 0;
-    hl = 0;
+    hl = s;
 
-    printf("\nCalling .EXIT\n");
-    rc = scall(0, &a, &bc, &de, &hl);
+    printf("\nCalling .PRINT\n");
+    rc = scall(3, &a, &bc, &de, &hl);
 
     printf("Returned %d\n", rc);
     printf("a  = %02x\n", a);
