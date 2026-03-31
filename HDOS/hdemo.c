4f707191ab39f5607139c7ec31bb174afd287e8f
Jeff Tranter <tranter@pobox.com>
2026-03-31 12:00:42 -0400

    Improvements to demo.

--- HDOS/hdemo.c
+++ HDOS/hdemo.c
@@ -5,37 +5,48 @@
 #include <time.h>
 #include <arch/hdos.h>
 
+/* Return H89 clock speed, using 2/4MHz speed mod. Returns speed in
+   MHz, i.e. 2 or 4, or -1 if unknown */
+
+int cpu_speed()
+{
+    switch (bpeek(0x2036) & 0x10) {
+    case 0x00:
+        return 2;
+        break;
+    case 0x10:
+        return 4;
+    default:
+        return -1;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    int ver, s, e;
+    int ver, s, e, c;
     struct tm *t;
     time_t tt;
 
     tt = time(0);
     t = localtime(&tt);
-    printf("tm_hour  = %d\n", t->tm_hour);
-    printf("tm_min   = %d\n", t->tm_min);
-    printf("tm_sec   = %d\n", t->tm_sec);
-    printf("tm_year  = %d\n", t->tm_year);
-    printf("tm_mon   = %d\n", t->tm_mon);
-    printf("tm_mday  = %d\n", t->tm_mday);
-    printf("tm_wday  = %d\n", t->tm_wday);
-    printf("tm_yday  = %d\n", t->tm_yday);
-    printf("tm_isdst = %d\n", t->tm_isdst);
-    printf("asctime  = %s", asctime(t));
-
     ver = hdos_ver();
-    printf("HDOS version is: %d.%d\n", ver / 16, ver % 16);
+    c = cpu_speed();
 
-    printf("Default device is: '%s'\n", default_device());
-
+    printf("HDOS version: %d.%d\n", ver / 16, ver % 16);
+    printf("Default device: '%s'\n", default_device());
+    printf("CPU speed: %d MHz\n", c);
     printf("Sleeping for 10 seconds...\n");
+
     s = clock();
-    sleep(10);
+    if (c == 4) {
+        sleep(20);
+    } else {
+        sleep(10);
+    }
     e = clock();
 
-    printf("clock ticks: %d\n", e - s);
-    printf("elapsed time: %d seconds\n", (e - s) / CLOCKS_PER_SEC);
+    printf("Clock ticks: %d\n", e - s);
+    printf("Elapsed time: %d seconds\n", (e - s) / CLOCKS_PER_SEC);
 
     return 0;
 }
