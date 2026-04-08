decffc94938c5a3f91b66e7fc177640400b705e9
Jeff Tranter <tranter@pobox.com>
2026-04-08 16:43:04 -0400

    Improve performance of scall function.

--- HDOS/hdosutil/scall.c
+++ HDOS/hdosutil/scall.c
@@ -1,6 +1,5 @@
-#include <arch/z80.h>
 #include <stdint.h>
-#include "hdosutil.h"
+#include <arch/z80.h>
 
 /* Wrapper for HDOS system call (scall). Pass in scall number and
    register values. Returns carry status (normally 1 for error, 0 for
@@ -8,28 +7,28 @@
 */
 int scall(uint8_t request, uint8_t *a, uint16_t *bc, uint16_t *de, uint16_t *hl)
 {
-    static Z80_registers regs;
-    extern uint8_t *CALLNO;
+    Z80_registers regs;
+    extern uint8_t CALLNO[];
 
-    // Write system call number after RST instruction
-    z80_bpoke(&CALLNO, request);
+    CALLNO[0] = request;
 
-    // Set up register parameters.
-    regs.Bytes.A = *a;
-    regs.UWords.BC = *bc;
-    regs.UWords.DE = *de;
-    regs.UWords.HL = *hl;
+    uint8_t a_val = *a;
+    uint16_t bc_val = *bc;
+    uint16_t de_val = *de;
+    uint16_t hl_val = *hl;
 
-    // Call SYSCALL routine below.
+    regs.Bytes.A = a_val;
+    regs.UWords.BC = bc_val;
+    regs.UWords.DE = de_val;
+    regs.UWords.HL = hl_val;
+
     AsmCall(&SYSCALL, &regs, REGS_MAIN, REGS_MAIN);
 
-    // Get back register values to return.
     *a = regs.Bytes.A;
     *bc = regs.Words.BC;
     *de = regs.Words.DE;
     *hl = regs.Words.HL;
 
-    // Return carry status
     return regs.Flags.C;
 }
 
